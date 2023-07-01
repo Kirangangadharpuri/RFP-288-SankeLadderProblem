a239af7888edf4249fb7ab5228234cae04f7cdf2
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-07-01 14:31:50 +0530

    [Kiran]Add\: Player till the 100th position

--- SnakeLadderProgram/SnakeLadderProgram/SnakeLadder.cs
+++ SnakeLadderProgram/SnakeLadderProgram/SnakeLadder.cs
@@ -9,40 +9,56 @@ namespace SnakeLadderProgram
     public class SnakeLadder
     {
         public const int CaseOfNoPlay = 0;
-        public const int CaseOfNoLadder = 1;
+        public const int CaseOfLadder = 1;
         public const int CaseOfSnake = 2;
+        public const int MaxPosition = 100;
+
         public void RollingDice()
         {
-            Console.WriteLine("Stert the Snake and Ladder Game");
-            Console.WriteLine("Single player at start Position 0");
-            Random random = new Random();
-            int RollDice = random.Next(1, 6);
-            Console.WriteLine("Player rolls the dice and get: " + RollDice);
-            Random random1 = new Random();
-            int option = random1.Next(3);
+            Console.WriteLine("Start the snake and ladder game");
+            Console.WriteLine("Single player at starit position 0");
+            Console.WriteLine(" ");
             int position = 0;
-
-
-            switch (option)
+            while (position != MaxPosition)
             {
-                case CaseOfNoPlay:
-                    {
-                        Console.WriteLine("No Play " + position);
-                        Console.WriteLine("Player stay at same position-");
-                        break;
-                    }
-                case CaseOfNoLadder:
-                    {
-                        Console.WriteLine("LADDER " + (RollDice + position));
-                        Console.WriteLine("Player move ahead {0} steps ", RollDice);
-                        break;
-                    }
-                case CaseOfSnake:
-                    {
-                        Console.WriteLine("Snake " + (RollDice - position));
-                        Console.WriteLine("Player move back {0} steps ", RollDice);
-                        break;
-                    }
+                Random random = new Random();
+                int RollDice = random.Next(1, 6);
+                Console.WriteLine("Player rolls the die and get: " + RollDice);
+                Random random1 = new Random();
+                int option = random1.Next(3);
+
+                switch (option)
+                {
+                    case CaseOfNoPlay:
+                        {
+                            Console.WriteLine("Optinc NO PLAY selected " + position);
+                            Console.WriteLine("Player stey at same postion");
+                            break;
+                        }
+
+                    case CaseOfLadder:
+                        {
+                            Console.WriteLine("Option LADDER selected " + position);
+                            Console.WriteLine("Player move ahead " + RollDice);
+                            position = RollDice + position;
+                            break;
+                        }
+
+                    case CaseOfSnake:
+                        {
+                            Console.WriteLine("Option SNAKE is selected " + (RollDice + position));
+                            Console.WriteLine("Player move behind by " + RollDice);
+                            position = position - RollDice;
+                            break;
+                        }
+                }
+                if (position < 0)
+                {
+                    position = 0;
+                    Console.WriteLine("Player have to restart from position " + position);
+                }
+                Console.WriteLine("Current position of player is " + position);
+                Console.WriteLine(" ");
             }
         }
     }
