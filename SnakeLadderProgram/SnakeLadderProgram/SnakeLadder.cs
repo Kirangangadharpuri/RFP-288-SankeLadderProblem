e9874e6fbaac1f52ecfe4418efec6004a09cf890
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-07-01 13:32:14 +0530

    [Kiran]Add\: single player get random position using switch case

--- SnakeLadderProgram/SnakeLadderProgram/SnakeLadder.cs
+++ SnakeLadderProgram/SnakeLadderProgram/SnakeLadder.cs
@@ -8,6 +8,9 @@ namespace SnakeLadderProgram
 {
     public class SnakeLadder
     {
+        public const int CaseOfNoPlay = 0;
+        public const int CaseOfNoLadder = 1;
+        public const int CaseOfSnake = 2;
         public void RollingDice()
         {
             Console.WriteLine("Stert the Snake and Ladder Game");
@@ -15,6 +18,32 @@ namespace SnakeLadderProgram
             Random random = new Random();
             int RollDice = random.Next(1, 6);
             Console.WriteLine("Player rolls the dice and get: " + RollDice);
+            Random random1 = new Random();
+            int option = random1.Next(3);
+            int position = 0;
+
+
+            switch (option)
+            {
+                case CaseOfNoPlay:
+                    {
+                        Console.WriteLine("No Play " + position);
+                        Console.WriteLine("Player stay at same position-");
+                        break;
+                    }
+                case CaseOfNoLadder:
+                    {
+                        Console.WriteLine("LADDER " + (RollDice + position));
+                        Console.WriteLine("Player move ahead {0} steps ", RollDice);
+                        break;
+                    }
+                case CaseOfSnake:
+                    {
+                        Console.WriteLine("Snake " + (RollDice - position));
+                        Console.WriteLine("Player move back {0} steps ", RollDice);
+                        break;
+                    }
+            }
         }
     }
 }
