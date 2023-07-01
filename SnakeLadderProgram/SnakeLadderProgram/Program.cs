a8598d139d05fcb4aac5579ff71a6ff46e8a3997
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-07-01 13:23:24 +0530

    [Kiran]Add\: single player started the game at zero position

---
+++ SnakeLadderProgram/SnakeLadderProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Snake and Ladder Program");
+            SnakeLadder snakeLadder = new SnakeLadder();
+            snakeLadder.RollingDice();
+        }
+    }
+}
---
+++ SnakeLadderProgram/SnakeLadderProgram/SnakeLadder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProgram
+{
+    public class SnakeLadder
+    {
+        public void RollingDice()
+        {
+            Console.WriteLine("Stert the Snake and Ladder Game");
+            int position = 0;
+            Console.WriteLine("Single player at start Position 0");
+        }
+    }
+}
